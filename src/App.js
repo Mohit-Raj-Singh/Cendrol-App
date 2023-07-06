f79d51a1aea124e2b6fc5eb0665b34c144da8ac5
Mohit Raj Singh <mohit94e@gmail.com>
2023-07-06 21:58:02 +0530

    final submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { ChuckNorries } from './Components/ChuckNorries';
 
--- src/Components/ChuckNorries.jsx
+++ src/Components/ChuckNorries.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ChuckNorries.css";
+import axios from "axios";
 
 export const ChuckNorries = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [name, setName] = useState({});
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.chucknorris.io/jokes/categories"
+      );
+      setData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const openModal = () => {
     setIsOpen(true);
@@ -12,36 +30,49 @@ export const ChuckNorries = () => {
     setIsOpen(false);
   };
 
-  return (
-    <div className="mainBox">
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
-      <div className="container" onClick={openModal}></div>
+  const handleClick = async (el) => {
+    try {
+      const response = await axios.get(
+        `https://api.chucknorris.io/jokes/random?category=${el}`
+      );
+      setName(response.data);
+    } catch (err) {
+      console.log(err);
+    }
 
-      {isOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <h2>Modal Title</h2>
-            <p>This is the modal content.</p>
+    openModal();
+  };
+
+  return (
+    <>
+      <h1 style={{ color: "#25b964" }}>Chuck Norries</h1>
+      <div className="mainBox">
+        {data.map((el) => (
+          <div
+            onClick={() => handleClick(el)}
+            className="container"
+            key={el.id}
+          >
+            <div className="nameHead">{el}</div>
+            <div className="content">Unlimited Jokes on {el}</div>
           </div>
-        </div>
-      )}
-    </div>
+        ))}
+
+        {isOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>
+                &times;
+              </span>
+              <h1>{name.categories[0]}</h1>
+              <div className="modelBox">
+                <h2>{name.value}</h2>
+                <button>Next Joke</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
